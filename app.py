abdc6daa26853da0c66eb3dd0d9e788b31bd7ec6
fazalq2 <141595141+fazalq2@users.noreply.github.com>
2024-10-06 18:41:46 +0500

    added table name to users

--- app.py
+++ app.py
@@ -46,6 +46,7 @@ Bootstrap(app)
 
 # Data Base Models
 class User(UserMixin, db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     email = db.Column(db.String(150), unique=True, nullable=False)
