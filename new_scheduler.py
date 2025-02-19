91ea432b0bf8c69277885f23b5b5008f452c1498
Asadus16 <asadslayer999@gmail.com>
2025-02-19 21:13:29 +0500

    new

--- new_scheduler.py
+++ new_scheduler.py
@@ -2,6 +2,10 @@ import os
 # import requests  # (remove if you're not using requests for anything else)
 from app import app, fetch_shipping_data, fetch_usda_daily_data, fetch_daily_data
 from usda import fetch_usda_terminal_data
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # This loads variables from .env into the environment
 
 
 def execute_scheduled_functions():
