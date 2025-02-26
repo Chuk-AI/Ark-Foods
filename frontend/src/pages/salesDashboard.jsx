861843fbab6610e34ef786be0c432bc066be27c8
Asadus16 <asadslayer999@gmail.com>
2025-02-26 15:02:32 +0500

    new

--- frontend/src/pages/salesDashboard.jsx
+++ frontend/src/pages/salesDashboard.jsx
@@ -23,7 +23,7 @@ function SalesDashboard() {
   // const [commodity, setCommodity] = useState('Anaheim');
   const [shippingPrices, setShippingPrices] = useState({});
 
-  const [source, setSource] = useState('both');
+  const [source, setSource] = useState('Both');
   const [last7Days, setLast7Days] = useState(false);
   const [bestMarketData, setBestMarketData] = useState([]);
   // const [chartInstance, setChartInstance] = useState(null);
